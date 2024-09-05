0a7a8643bffd29760308759c09a9f1069c22422f
kory33 <korygm33@gmail.com>
2024-09-06 01:20:57 +0900

    feat: implement the first (probably working) simplest form of otel4s integration

---
+++ modules/integrations/otel4s/src/main/scala/io/github/kory33/tracing_instrument/otel4s/instrumentF.scala
@@ -0,0 +1,62 @@
+package io.github.kory33.tracing_instrument.otel4s
+
+import scala.compiletime.summonInline
+import scala.language.experimental
+import scala.annotation.{MacroAnnotation, experimental}
+import scala.quoted.*
+import org.typelevel.otel4s.trace.Tracer
+
+@experimental class instrumentF extends MacroAnnotation {
+  override def transform(using Quotes)(
+      definition: quotes.reflect.Definition,
+      companion: Option[quotes.reflect.Definition]
+  ): List[quotes.reflect.Definition] = {
+    import quotes.reflect.*
+
+    definition match {
+      case DefDef(defName, params, returnType, Some(definingTerm)) =>
+        returnType match
+          case Applied(fTree, List(retInnerType: TypeTree)) =>
+            fTree.tpe.asType match
+              case '[type f[_] f] =>
+                retInnerType.tpe.asType match
+                  case '[ret] =>
+                    List(
+                      DefDef.copy(definition)(
+                        defName,
+                        params,
+                        returnType,
+                        Some(
+                          newDefinition[f, ret](
+                            definingTerm.asExprOf[f[ret]],
+                            defName
+                          ).asTerm
+                        )
+                      )
+                    )
+          case _ =>
+            report.errorAndAbort(
+              "Expected a return type of the form f[r], where f is a type constructor and r is a type."
+            )
+
+      // Error cases
+      case DefDef(_, _, _, None) =>
+        report.errorAndAbort(
+          "@instrument has been applied to a definition without a defining term."
+        )
+      case _ =>
+        report.errorAndAbort(
+          "@instrument has been applied to an unsupported definition. Only method definitions are supported."
+        )
+    }
+  }
+
+  private def newDefinition[F[_]: Type, RetType: Type](using Quotes)(
+      definingTerm: Expr[F[RetType]],
+      defName: String
+  ): Expr[F[RetType]] = '{
+    summonInline[Tracer[F]].span(${ Expr(defName) }).use { _ =>
+      ${ definingTerm.asExprOf[F[RetType]] }
+    }
+  }
+}
